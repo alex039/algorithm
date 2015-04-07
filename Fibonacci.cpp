ab35cd475bf61c3ec1f403a245ba3db9b213cd6b
alex_039@163.com <alex_039@163.com>
2015-04-07 11:02:01 +0800

    Fibonacci algorithm

---
+++ Fibonacci.cpp
@@ -0,0 +1,52 @@
+//F0 = 0, F1 = 1, F2 = 1, Fn=Fn-1+Fn-2
+//(Fn+1, Fn) (1, 1)n
+//(Fn, Fn-1)=(1, 0)
+#include <memory.h>
+void multiply(const int(*a)[2], const int(*b)[2], int(*Result)[2]);
+void PowerMatrix(int n, int(*Resutl)[2]);
+const int elementMatrix[2][2] = {1,1,1,0};
+int Fibonacci(int n)
+{
+
+	if (n == 0)
+	{
+		return 0;
+	}
+	int Result[2][2] = { 1, 1, 1, 0 };
+	PowerMatrix(n, Result);
+	return Result[0][1];
+}
+void PowerMatrix(int n, int(*Result)[2])
+{
+	if (n > 1)
+	{
+		PowerMatrix(n / 2, Result);
+
+		if (n % 2 == 0)
+		{
+			multiply(Result, Result, Result);
+		}
+		else
+		{
+			multiply(Result, Result, Result);
+			multiply(Result, elementMatrix, Result);
+		}
+	}
+}
+
+void multiply(const int(*a)[2], const int(*b)[2], int(*Result)[2])
+{
+	int copyA[2][2] = {0};
+	int copyB[2][2] = {0};
+	
+	memcpy(copyA,a,4*sizeof(int));
+	memcpy(copyB, b, 4 * sizeof(int));
+
+	for (int i = 0; i < 2; i++)
+	{
+		for (int j = 0; j < 2; j++)
+		{
+			Result[i][j] = copyA[i][0] * copyB[0][j] + copyA[i][1] * copyB[1][j];
+		}
+	}
+}
